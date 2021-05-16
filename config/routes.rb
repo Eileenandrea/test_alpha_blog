7a9d6462af56a430328c137cea6c19025b89a08d
Eileenandrea <eileendeguzman.edg@gmail.com>
2021-05-16 18:03:02 +0800

    adding edit and update

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :articles, only: [:show]
+
   get '/articles', to: 'articles#index'
+  get '/articles/new', to: 'articles#new', as: 'new_article'
+  post '/articles', to: 'articles#create', as: 'create_article' 
+  get '/articles/:id', to: 'articles#show', as: 'show_article'
+  get '/articles/:id/edit', to: 'articles#edit', as: 'edit_article'
+  patch '/articles/:id', to: 'articles#update', as: 'update_article'
 end
